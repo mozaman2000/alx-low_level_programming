b51439f6d3dc9a8c78b9466a6f4cf44f1b7bcff7
mozaman2000 <moaazhany8@gmail.com>
2023-06-16 20:23:42 -0700

    added third task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'abcdefghijklmnopqrstuvwxyz';
+/* your code goes there */
+	putchar(alpha);
+return (0);
