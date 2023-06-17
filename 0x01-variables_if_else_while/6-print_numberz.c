fe85983e5af2349dbebd3ad5e35cf77246f849f8
mozaman2000 <moaazhany8@gmail.com>
2023-06-17 00:26:47 -0700

    d

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums = 0;
+
+	while (nums < 10)
+	{
+		putchar(nums);
+		++nums;
+	}
+	putchar('\n');
+	return (0);
+}
+
