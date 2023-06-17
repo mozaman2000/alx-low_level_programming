10766211816290c15f8798275832abb7fc43c72d
mozaman2000 <moaazhany8@gmail.com>
2023-06-16 18:36:51 -0700

    added first task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,12 @@
 /* betty style doc for function main goes there */
 
 /**
- * main_function - This is the main function
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
