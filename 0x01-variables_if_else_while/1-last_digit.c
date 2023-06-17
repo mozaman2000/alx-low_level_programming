4675460fce6eb9322bfb28f3a10d5d5d20ef4719
mozaman2000 <moaazhany8@gmail.com>
2023-06-16 19:24:45 -0700

    added second task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,31 +6,32 @@
 /* betty style doc for function main goes there */
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: 'the program's description'
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ * * main - Entry point
+ * *
+ * * Description: 'the program's description'
+ * *
+ * * Return: Always 0 (Success)
+**/
 int main(void)
 {
-        int n;
-        int n_last_digit;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
+	int n;
+	int n_last_digit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 /* your code goes there */
-        n_last_digit = n %10;
+	n_last_digit = n %10;
+	
 	if (n_last_digit > 5)
 	{
-        printf("Last digit of%iis%iand is greater than 5\n", n, n_last_digit);
-        }
-        if (n_last_digit == 0)
-        {
-        printf("Last digit of%iis%iand is 0\n", n, n_last_digit);
-        }
-        if (n < 6 && n == 0)
-        {
-        printf("Last digit of%iis%iand is less than 6 and not 0\n", n, n_last_digit);
-        }
+	printf("Last digit of %i is %i and is greater than 5\n", n, n_last_digit);
+	}
+	if (n_last_digit == 0)
+	{
+	printf("Last digit of %i is %i and is 0\n", n, n_last_digit);
+	}
+	if (n < 6 && n == 0)
+	{
+	printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n_last_digit);
+	}
 return (0);
 }
