906ba315980ef747646b21cf3c53e69dd3641c07
mozaman2000 <moaazhany8@gmail.com>
2023-06-17 00:04:13 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nums = 0;
+	
+	while(nums < 10)
+	{
+		printf(nums);
+		++nums;
+	}
+	printf('\n');
+	return (0);
+}
+
