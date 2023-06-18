7f3c33bd6592d98f8e0ea4503d801038d5d8b498
mozaman2000 <moaazhany8@gmail.com>
2023-06-17 22:01:43 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
+ * main - description prints "Programming is like building a multilingual
  *  puzzle"
  *  return 0.
  */
@@ -11,5 +11,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
