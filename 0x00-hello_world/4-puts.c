1e9bd1e6a1ccb4e842f248645199a865b0429491
mozaman2000 <moaazhany8@gmail.com>
2023-06-17 22:08:15 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - prints "Programming is like building a multilingual
  *  puzzle"
- *  return always 0.
+ *  Return: always 0.
  */
 
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
- *  puzzle"
- *  return always 0.
+ * main - prints Programming is like building a multilingual
+ *  puzzle
+ *  Return: always 0
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
