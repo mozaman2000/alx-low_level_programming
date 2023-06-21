58803a397321d4a0e91ed3e8d3f5ab916123a936
mozaman2000 <moaazhany8@gmail.com>
2023-06-21 15:13:22 -0700

    all tasks added

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,8 +4,10 @@
 * Return: 0
 */
 int main(void)
+
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
 i = 0;
 /*while (i < 10)*/
@@ -13,6 +15,7 @@ i = 0;
 /*putchar(i);*/
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
 
