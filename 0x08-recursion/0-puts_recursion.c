97a5ae0b8b38c62b4cbaac28293b79701f080c3b
mozaman2000 <moaazhany8@gmail.com>
2023-07-05 05:41:01 -0700

    d

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <unistd.h>
+#include <"_putchar.c">
+/**
+ * _puts_recursion - a function that copies memory area
+ *@s: a string
+ *
+ *Return: copied memory with n bytes changed
+ */
+int _putchar(char s)
+{
+	return (write(1, &c, 1));
+}
+void _puts_recursion(char s);
+{
+	_putchar("%d", s)
+
+	return (0);
+}
+
